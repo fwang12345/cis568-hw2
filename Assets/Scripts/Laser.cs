4dc46339ca34b8ba3185ccae21f6b059848d5f9a
Frankie Wang <frankiewang19@gmail.com>
2021-01-28 19:46:34 -0500

    Added laserbase and laser shooting

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Vector3 thrust;
+    
+    void Start()
+    {
+        thrust.z = 800.0f;
+        GetComponent<Rigidbody>().drag = 0;
+        GetComponent<Rigidbody>().AddRelativeForce(thrust);
+    }
+}
---
+++ Assets/Scripts/LaserBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBase : MonoBehaviour
+{
+    public float velocity;
+    public GameObject laser;
+    void Start()
+    {
+        velocity = 10.0f;
+    }
+    void Update() 
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // Spawn laser
+            Vector3 spawnPos = gameObject.transform.position;
+            spawnPos.z += 1.0f;
+            GameObject obj = Instantiate(laser, spawnPos, Quaternion.identity) as GameObject;
+            Laser b = obj.GetComponent<Laser>();
+        }
+    }
+    void FixedUpdate()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        if (direction > 0)
+        {
+            //GetComponent<Rigidbody>().AddForce(new Vector3(velocity, 0, 0));
+            GetComponent<Rigidbody>().velocity = new Vector3(velocity, 0, 0);
+        }
+        else if (direction < 0)
+        {
+            //GetComponent<Rigidbody>().AddForce(new Vector3(-velocity, 0, 0));
+            GetComponent<Rigidbody>().velocity = new Vector3(-velocity, 0, 0);
+        } else {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
